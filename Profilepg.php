d74c58f8a92848eda92f1f39382591f6ac4cfef5
athulsebastiant <athulsebastiant.com>
2024-09-23 20:39:19 +0530

    Added latest db and minor changes

--- Profilepg.php
+++ Profilepg.php
@@ -22,7 +22,7 @@ $user = $result->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Details</title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <title>Personal Accounting - Home</title>
+    <title>Personal Accounting - Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="Syles.css">
     <style>
